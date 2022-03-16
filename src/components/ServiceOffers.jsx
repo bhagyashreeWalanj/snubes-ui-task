d6ac3c74a10378207fcb85b4b0d01a10d20a73c4
bhagyashreeWalanj <shree.6490@gmail.com>
2022-03-16 13:05:19 +0100

    shown responsive by tailwind css

--- src/components/ServiceOffers.jsx
+++ src/components/ServiceOffers.jsx
@@ -1,33 +1,23 @@
 import React from 'react'
 import services from '../__mockups/servicesOffer.json';
-import { Figure, Container, Row, Col } from 'react-bootstrap'
 
 export default function ServiceOffers() {
 
   return (
     <div id="servicesOffers" className="flex justify-center items-center mx-auto py-10 text-black border-y-4 border-zinc-100">
-      <Container>
-        <Row>
-        {services.servicesOffers.map((service, index) => {
-           return( <Col sm key={index} className="text-center">
-            <Figure>
-              <Figure.Image
-                className="mx-auto w-20 h-20"
-                alt="Signup free"
-                src={service.image}
-              />
-              <Figure.Caption className="text-center">
-                <h3 className="font-bold md:text-center text-xl mb-2">
-                  {service.name}
-                </h3>
-                <p> {service.description}</p>
-              </Figure.Caption>
-            </Figure>
-          </Col>)
-        })}
-        
-        </Row>
-      </Container>
+      <div className="flex items-center justify-center">
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 p-2 text-center justify-center">
+          {services.servicesOffers.map((service, index) => {
+            return (<div key={index} className=" text-gray-500 text-center p-10 rounded-lg">
+              <img src={service.image} alt="signup" className="mx-auto w-20 h-20" />
+              <h3 className="font-bold md:text-center text-xl mb-2">
+                {service.name}
+              </h3>
+              <p> {service.description}</p></div>)
+          })
+          }
+        </div>
+      </div>
     </div>
   )
 }
